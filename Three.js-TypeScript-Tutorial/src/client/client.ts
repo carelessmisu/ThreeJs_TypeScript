8b7e76a39645d5d2c298f348b465fa3eaa8d84af
yuta misu <missun1201@me.com>
2022-12-01 16:13:19 +0900

    add Stats Panel

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 
@@ -35,18 +36,23 @@ function onWindowResize() {
     render()
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+function animate() {
+    requestAnimationFrame(animate)
 
-//     render()
-// }
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    render()
+
+    stats.update()
+}
 
 function render() {
     renderer.render(scene, camera)
 }
 
-//animate()
-render()
+animate()
+//render()
